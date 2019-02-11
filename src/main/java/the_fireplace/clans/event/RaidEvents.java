b3ea848be84d4d10ba100312e2dd930f8b68ff98
The_Fireplace <the.f1repl4ce@gmail.com>
2019-02-10 19:57:37 -0600

    Fix slime block+piston dupe bug

--- src/main/java/the_fireplace/clans/event/RaidEvents.java
+++ src/main/java/the_fireplace/clans/event/RaidEvents.java
@@ -1,10 +1,16 @@
 package the_fireplace.clans.event;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import net.minecraft.block.BlockPistonBase;
+import net.minecraft.block.BlockPistonMoving;
+import net.minecraft.block.BlockSlime;
 import net.minecraft.block.material.EnumPushReaction;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Blocks;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityPiston;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.chunk.Chunk;
@@ -21,6 +27,7 @@ import the_fireplace.clans.clan.Clan;
 import the_fireplace.clans.clan.ClanCache;
 import the_fireplace.clans.raid.RaidingParties;
 
+import java.util.HashMap;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -72,33 +79,56 @@ public class RaidEvents {
 		}
 	}
 
+	private static HashMap<BlockPos, Boolean> phases = Maps.newHashMap();
+
 	@SubscribeEvent
 	public static void onNeighborNotify(BlockEvent.NeighborNotifyEvent event) {
 		if(!event.getWorld().isRemote) {
 			IBlockState state = event.getState();
 			if (state.getBlock() instanceof BlockPistonBase) {
-				if (event.getState().getProperties().containsKey(BlockPistonBase.FACING)) {
+				if (event.getState().getProperties().containsKey(BlockPistonBase.FACING) && event.getState().getProperties().containsKey(BlockPistonBase.EXTENDED)) {
 					Comparable facing = event.getState().getProperties().get(BlockPistonBase.FACING);
 					Comparable extended = event.getState().getProperties().get(BlockPistonBase.EXTENDED);
 					BlockPos oldPos = event.getPos();
 					BlockPos newPos = event.getPos();
+					if(!phases.containsKey(event.getPos()))
+						phases.put(event.getPos(), !(Boolean) extended);
 
-					if (facing instanceof EnumFacing && extended instanceof Boolean) {
-						int pushRange = 0;
-						BlockPos testPos = event.getPos();
-						for(int i=1;i<14;i++)
-							if(event.getWorld().getBlockState(testPos.offset((EnumFacing) facing, i)).getBlock() == Blocks.AIR || event.getWorld().getBlockState(testPos.offset((EnumFacing) facing, i)).getPushReaction().equals(EnumPushReaction.DESTROY)) {
-								pushRange = i;
-								break;
-							}
-						//TODO handle blocks when pushing slime block
+					if (facing instanceof EnumFacing && extended instanceof Boolean && phases.get(event.getPos()) == extended) {
 						if ((Boolean) extended) {
+							int pushRange = 0;
+							BlockPos testPos = event.getPos();
+							for(int i=1;i<14;i++)
+								if(event.getWorld().getBlockState(testPos.offset((EnumFacing) facing, i)).getBlock() == Blocks.AIR || event.getWorld().getBlockState(testPos.offset((EnumFacing) facing, i)).getPushReaction().equals(EnumPushReaction.DESTROY)) {
+									pushRange = i;
+									break;
+								}
 							for(int i=pushRange-1;i>1;i--) {
 								newPos = event.getPos().offset((EnumFacing) facing, i);
 								oldPos = event.getPos().offset((EnumFacing) facing, i-1);
 								Chunk oldChunk = event.getWorld().getChunk(oldPos);
 								Chunk newChunk = event.getWorld().getChunk(newPos);
 								shiftBlocks(event, oldPos, newPos, oldChunk, newChunk);
+								TileEntity piston = event.getWorld().getTileEntity(newPos);
+								if(piston instanceof TileEntityPiston && ((TileEntityPiston)piston).getPistonState().getBlock() instanceof BlockSlime) {
+									switch((EnumFacing) facing) {
+										case UP:
+										case DOWN:
+											for(EnumFacing shiftDir: Lists.newArrayList(EnumFacing.NORTH, EnumFacing.SOUTH, EnumFacing.EAST, EnumFacing.WEST))
+												doSlimePush(event, (EnumFacing) facing, newPos, shiftDir);
+											break;
+										case EAST:
+										case WEST:
+											for(EnumFacing shiftDir: Lists.newArrayList(EnumFacing.NORTH, EnumFacing.SOUTH, EnumFacing.UP, EnumFacing.DOWN))
+												doSlimePush(event, (EnumFacing) facing, newPos, shiftDir);
+											break;
+										case NORTH:
+										case SOUTH:
+											for(EnumFacing shiftDir: Lists.newArrayList(EnumFacing.UP, EnumFacing.DOWN, EnumFacing.EAST, EnumFacing.WEST))
+												doSlimePush(event, (EnumFacing) facing, newPos, shiftDir);
+											break;
+									}
+								}
 							}
 						} else if(state.getBlock().equals(Blocks.STICKY_PISTON)) {
 							oldPos = oldPos.offset((EnumFacing) facing, 2);
@@ -106,11 +136,63 @@ public class RaidEvents {
 							Chunk oldChunk = event.getWorld().getChunk(oldPos);
 							Chunk newChunk = event.getWorld().getChunk(newPos);
 							shiftBlocks(event, oldPos, newPos, oldChunk, newChunk);
+							if(event.getWorld().getBlockState(newPos) instanceof BlockSlime) {
+								switch((EnumFacing) facing) {
+									case UP:
+									case DOWN:
+										for(EnumFacing shiftDir: Lists.newArrayList(EnumFacing.NORTH, EnumFacing.SOUTH, EnumFacing.EAST, EnumFacing.WEST))
+											doSlimePull(event, (EnumFacing) facing, newPos, shiftDir);
+										break;
+									case EAST:
+									case WEST:
+										for(EnumFacing shiftDir: Lists.newArrayList(EnumFacing.NORTH, EnumFacing.SOUTH, EnumFacing.UP, EnumFacing.DOWN))
+											doSlimePull(event, (EnumFacing) facing, newPos, shiftDir);
+										break;
+									case NORTH:
+									case SOUTH:
+										for(EnumFacing shiftDir: Lists.newArrayList(EnumFacing.UP, EnumFacing.DOWN, EnumFacing.EAST, EnumFacing.WEST))
+											doSlimePull(event, (EnumFacing) facing, newPos, shiftDir);
+										break;
+								}
+							}
 						}
+						phases.put(event.getPos(), !phases.get(event.getPos()));
 					}
 				}
 			}
 		}
+	}
+
+	private static void doSlimePush(BlockEvent.NeighborNotifyEvent event, EnumFacing facing, BlockPos newPos, EnumFacing shiftDir) {
+		BlockPos testPos = newPos.offset(shiftDir);
+		BlockPos oldPos;
+		if(event.getWorld().getBlockState(testPos).getPushReaction().equals(EnumPushReaction.NORMAL) || event.getWorld().getBlockState(testPos).getPushReaction().equals(EnumPushReaction.IGNORE))
+			return;
+		int pushRange2 = 0;
+		for(int j=1;j<14;j++)
+			if(event.getWorld().getBlockState(testPos.offset(facing, j)).getBlock() == Blocks.AIR || event.getWorld().getBlockState(testPos.offset(facing, j)).getPushReaction().equals(EnumPushReaction.DESTROY)) {
+				pushRange2 = j;
+				break;
+			}
+		for(int j=pushRange2;j>0;j--) {
+			newPos = testPos.offset(facing, j - 1);
+			oldPos = testPos.offset(facing, j - 2);
+			Chunk oldChunk2 = event.getWorld().getChunk(oldPos);
+			Chunk newChunk2 = event.getWorld().getChunk(newPos);
+			shiftBlocks(event, oldPos, newPos, oldChunk2, newChunk2);
+		}
+	}
+
+	private static void doSlimePull(BlockEvent.NeighborNotifyEvent event, EnumFacing facing, BlockPos newPos, EnumFacing shiftDir) {
+		BlockPos testPos = newPos.offset(shiftDir);
+		BlockPos oldPos;
+		if(event.getWorld().getBlockState(testPos).getPushReaction().equals(EnumPushReaction.BLOCK) || event.getWorld().getBlockState(testPos).getPushReaction().equals(EnumPushReaction.IGNORE) || event.getWorld().getBlockState(testPos).getPushReaction().equals(EnumPushReaction.PUSH_ONLY))
+			return;
+		newPos = testPos.offset(facing);
+		oldPos = testPos.offset(facing, 2);
+		Chunk oldChunk2 = event.getWorld().getChunk(oldPos);
+		Chunk newChunk2 = event.getWorld().getChunk(newPos);
+		shiftBlocks(event, oldPos, newPos, oldChunk2, newChunk2);
 	}
 
 	private static void shiftBlocks(BlockEvent.NeighborNotifyEvent event, BlockPos oldPos, BlockPos newPos, Chunk oldChunk, Chunk newChunk) {
