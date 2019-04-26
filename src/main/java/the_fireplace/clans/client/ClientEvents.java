72144e976136bf6a7b32e94111dc06914d649497
The_Fireplace <the.f1repl4ce@gmail.com>
2019-04-26 14:29:42 -0500

    Save config when modified in the config gui. Closes #45

---
+++ src/main/java/the_fireplace/clans/client/ClientEvents.java
@@ -0,0 +1,18 @@
+package the_fireplace.clans.client;
+
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import the_fireplace.clans.Clans;
+
+@Mod.EventBusSubscriber(value = Side.CLIENT, modid = Clans.MODID)
+public class ClientEvents {
+    @SubscribeEvent
+    public static void configChanged(ConfigChangedEvent event) {
+        if (event.getModID().equals(Clans.MODID))
+            ConfigManager.sync(Clans.MODID, Config.Type.INSTANCE);
+    }
+}
