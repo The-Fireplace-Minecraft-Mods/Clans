2a3efbb3000399e85b5ba459174267ff77c3aed8
The_Fireplace <the.f1repl4ce@gmail.com>
2019-05-15 12:37:56 -0500

    Fix claiming crash when a clan doesn't have a home set, fix changelog including info that wouldn't be relevant on this branch

--- src/main/java/the_fireplace/clans/commands/CommandClan.java
+++ src/main/java/the_fireplace/clans/commands/CommandClan.java
@@ -881,7 +881,7 @@ public class CommandClan {
                     } else if(Clans.cfg.minClanHomeDist > 0 && Clans.cfg.initialClaimSeparationMultiplier > 0) {
                         boolean inClanHomeRange = false;
                         for(Map.Entry<NewClan, BlockPos> pos: ClanCache.getClanHomes().entrySet())
-                            if(pos.getValue().getDistance(context.getSource().asPlayer().getPosition().getX(), context.getSource().asPlayer().getPosition().getY(), context.getSource().asPlayer().getPosition().getZ()) < Clans.cfg.minClanHomeDist*Clans.cfg.initialClaimSeparationMultiplier)
+                            if(!pos.getKey().getClanId().equals(clan.getClanId()) && pos.getKey().hasHome() && pos.getValue() != null && pos.getValue().getDistance(context.getSource().asPlayer().getPosition().getX(), context.getSource().asPlayer().getPosition().getY(), context.getSource().asPlayer().getPosition().getZ()) < Clans.cfg.minClanHomeDist*Clans.cfg.initialClaimSeparationMultiplier)
                                 inClanHomeRange = true;
                         if(inClanHomeRange) {
                             if(Clans.cfg.enforceInitialClaimSeparation)
