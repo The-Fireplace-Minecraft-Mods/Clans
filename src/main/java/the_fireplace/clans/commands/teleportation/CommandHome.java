dbff2b5e4badd97da0225ae0eaea3b0015eb560e
Matthew Green <LordFusion@users.noreply.github.com>
2019-05-19 18:14:59 -0500

    Resolve Issue #50

--- src/main/java/the_fireplace/clans/commands/teleportation/CommandHome.java
+++ src/main/java/the_fireplace/clans/commands/teleportation/CommandHome.java
@@ -31,27 +31,27 @@ public class CommandHome extends ClanSubCommand {
 	public EnumRank getRequiredClanRank() {
 		return EnumRank.MEMBER;
 	}
-
+	
 	@Override
 	public int getMinArgs() {
 		return 0;
 	}
-
+	
 	@Override
 	public int getMaxArgs() {
 		return 0;
 	}
-
+	
 	@Override
 	public String getUsage(ICommandSender sender) {
 		return "/clan home";
 	}
-
+	
 	@Override
 	public void run(@Nullable MinecraftServer server, EntityPlayerMP sender, String[] args) {
 		BlockPos home = selectedClan.getHome();
 		int playerDim = sender.dimension;
-
+		
 		int cooldown = CapHelper.getPlayerClanCapability(sender).getCooldown();
 		if(cooldown <= 0) {
 			if (!selectedClan.hasHome() || home == null)
@@ -65,44 +65,55 @@ public class CommandHome extends ClanSubCommand {
 		} else
 			sender.sendMessage(new TextComponentString("You cannot use this command until your cooldown runs out in "+cooldown+" seconds.").setStyle(TextStyles.RED));
 	}
-
+	
 	public static void teleportHome(EntityPlayerMP player, NewClan playerClan, BlockPos home, int playerDim) {
 		home = getSafeExitLocation(Objects.requireNonNull(player.getServer()).getWorld(playerClan.getHomeDim()), home, 5);
-		if (playerDim == playerClan.getHomeDim() || player.changeDimension(playerClan.getHomeDim()) != null) {
+		if (playerDim == playerClan.getHomeDim()) {
 			if (!player.attemptTeleport(home.getX(), home.getY(), home.getZ())) {
 				player.sendMessage(new TextComponentString("Error teleporting to clan home. Ensure that it is not blocked.").setStyle(TextStyles.RED));
 				if (playerDim != player.dimension && player.changeDimension(playerDim) == null)
 					player.sendMessage(new TextComponentString("Error teleporting you back to the dimension you were in.").setStyle(TextStyles.RED));
 			} else
 				CapHelper.getPlayerClanCapability(player).setCooldown(Clans.cfg.clanHomeCooldownTime);
-		} else
-			player.sendMessage(new TextComponentString("Error teleporting to clan home dimension.").setStyle(TextStyles.RED));
+		} else {
+			player.setPortal(player.getPosition());
+			if (player.changeDimension(playerClan.getHomeDim()) != null) {
+				if (!player.attemptTeleport(home.getX(), home.getY(), home.getZ())) {
+					player.sendMessage(new TextComponentString("Error teleporting to clan home. Ensure that it is not blocked.").setStyle(TextStyles.RED));
+					if (playerDim != player.dimension && player.changeDimension(playerDim) == null)
+						player.sendMessage(new TextComponentString("Error teleporting you back to the dimension you were in.").setStyle(TextStyles.RED));
+				} else
+					CapHelper.getPlayerClanCapability(player).setCooldown(Clans.cfg.clanHomeCooldownTime);
+			} else {
+				player.sendMessage(new TextComponentString("Error teleporting to clan home dimension.").setStyle(TextStyles.RED));
+			}
+		}
 	}
-
+	
 	private static BlockPos getSafeExitLocation(World worldIn, BlockPos pos, int tries) {
 		int i = pos.getX();
 		int j = pos.getY();
 		int k = pos.getZ();
-
+		
 		for (int l = 0; l <= 1; ++l) {
 			int i1 = i - Integer.compare(pos.getX(), 0) * l - 1;
 			int j1 = k - Integer.compare(pos.getZ(), 0) * l - 1;
 			int k1 = i1 + 2;
 			int l1 = j1 + 2;
-
+			
 			for (int i2 = i1; i2 <= k1; ++i2) {
 				for (int j2 = j1; j2 <= l1; ++j2) {
 					BlockPos blockpos = new BlockPos(i2, j, j2);
-
+					
 					if (hasRoomForPlayer(worldIn, blockpos) || --tries <= 0)
 						return blockpos;
 				}
 			}
 		}
-
+		
 		return pos;
 	}
-
+	
 	private static boolean hasRoomForPlayer(World worldIn, BlockPos pos) {
 		return worldIn.getBlockState(pos.down()).isTopSolid() && !worldIn.getBlockState(pos).getMaterial().isSolid() && !worldIn.getBlockState(pos.up()).getMaterial().isSolid();
 	}
