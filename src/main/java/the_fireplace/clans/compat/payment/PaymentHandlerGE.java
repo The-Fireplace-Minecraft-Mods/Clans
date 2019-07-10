7d67d9017bbe900dd16e4ac467e714cad80a32e5
The_Fireplace <the.f1repl4ce@gmail.com>
2019-07-09 21:14:38 -0500

    Update Grand Economy

--- src/main/java/the_fireplace/clans/compat/payment/PaymentHandlerGE.java
+++ src/main/java/the_fireplace/clans/compat/payment/PaymentHandlerGE.java
@@ -3,11 +3,8 @@ package the_fireplace.clans.compat.payment;
 import the_fireplace.clans.clan.ClanCache;
 import the_fireplace.grandeconomy.GrandEconomy;
 import the_fireplace.grandeconomy.api.GrandEconomyApi;
-import the_fireplace.grandeconomy.econhandlers.ge.Account;
 import the_fireplace.grandeconomy.econhandlers.ge.GrandEconomyEconHandler;
 
-import java.io.IOException;
-import java.util.Objects;
 import java.util.UUID;
 
 public class PaymentHandlerGE implements IPaymentHandler {
@@ -16,11 +13,7 @@ public class PaymentHandlerGE implements IPaymentHandler {
 	public boolean deductAmount(long amount, UUID account) {
 		boolean ret = GrandEconomyApi.takeFromBalance(account, amount, false);
 		if(ret && ClanCache.getClanById(account) != null && GrandEconomy.economy instanceof GrandEconomyEconHandler)
-			try {
-				Objects.requireNonNull(Account.get(account)).writeIfChanged();
-			} catch(IOException e) {
-				e.printStackTrace();
-			}
+			GrandEconomyApi.forceSave(account);
 		return ret;
 	}
 
@@ -40,18 +33,13 @@ public class PaymentHandlerGE implements IPaymentHandler {
 	public boolean addAmount(long amount, UUID account) {
 		GrandEconomyApi.addToBalance(account, amount, false);
 		if(ClanCache.getClanById(account) != null && GrandEconomy.economy instanceof GrandEconomyEconHandler)
-			try {
-				Objects.requireNonNull(Account.get(account)).writeIfChanged();
-			} catch(IOException e) {
-				e.printStackTrace();
-				return false;
-			}
+			GrandEconomyApi.forceSave(account);
 		return true;
 	}
 
 	@Override
 	public void ensureAccountExists(UUID account) {
-		GrandEconomyApi.hasAccount(account);
+		GrandEconomyApi.ensureAccountExists(account);
 	}
 
 	@Override
