38e356577ecd3dcfee0aaae518319d1b2edfafc8
The_Fireplace <the.f1repl4ce@gmail.com>
2019-07-27 13:42:32 -0500

    Take borderlands into account when claiming and abandoning land

--- src/main/java/the_fireplace/clans/util/ClanManagementUtil.java
+++ src/main/java/the_fireplace/clans/util/ClanManagementUtil.java
@@ -35,46 +35,48 @@ public class ClanManagementUtil {
         Clan claimClan = ClanCache.getClanById(claimOwner);
         String clanCommandString = isOpclanCommand ? "opclan" : "clan";
         if(claimOwner != null && claimClan != null && (!force || claimOwner.equals(selectedClan.getClanId()))) {
-            if(claimOwner.equals(selectedClan.getClanId()))
+            if(!claimOwner.equals(selectedClan.getClanId())) {
+                sender.sendMessage(TranslationUtil.getTranslation(sender.getUniqueID(), "commands." + clanCommandString + ".claim.taken_other", claimClan.getClanName()).setStyle(TextStyles.RED));
+                return false;
+            } else if(!ClaimDataManager.getChunkPositionData(claimChunk).isBorderland()) {
                 sender.sendMessage(TranslationUtil.getTranslation(sender.getUniqueID(), "commands.clan.claim.taken", selectedClan.getClanName()).setStyle(TextStyles.YELLOW));
-            else
-                sender.sendMessage(TranslationUtil.getTranslation(sender.getUniqueID(), "commands."+clanCommandString+".claim.taken_other", claimClan.getClanName()).setStyle(TextStyles.RED));
-        } else {
-            if(claimClan != null) {//In this scenario, we are always forcing the claim, so we should refund the clan the land is being taken from
-                claimClan.refundClaim();
+                return false;
             }
-            if(selectedClan.isOpclan()) {
-                ClaimDataManager.swapChunk(claimChunk, claimOwner, selectedClan.getClanId());
-                sender.sendMessage(TranslationUtil.getTranslation(sender.getUniqueID(), "commands.clan.claim.success", selectedClan.getClanName()).setStyle(TextStyles.GREEN));
-                return true;
-            } else {
-                if (force || !Clans.getConfig().isForceConnectedClaims() || ChunkUtils.hasConnectedClaim(claimChunk, selectedClan.getClanId()) || selectedClan.getClaimCount() == 0) {
-                    if (force || Clans.getConfig().getMaxClaims() <= 0 || selectedClan.getClaimCount() < selectedClan.getMaxClaimCount()) {
-                        if (selectedClan.getClaimCount() > 0)
-                            claimChunk(sender, claimChunk, selectedClan, force);
-                        else if (Clans.getConfig().getMinClanHomeDist() > 0 && Clans.getConfig().getInitialClaimSeparationMultiplier() > 0) {
-                            boolean inClanHomeRange = false;
-                            for (Map.Entry<Clan, BlockPos> pos : ClanCache.getClanHomes().entrySet())
-                                if (!pos.getKey().getClanId().equals(selectedClan.getClanId()) && pos.getKey().hasHome() && pos.getValue() != null && pos.getValue().getDistance(sender.getPosition().getX(), sender.getPosition().getY(), sender.getPosition().getZ()) < Clans.getConfig().getMinClanHomeDist() * Clans.getConfig().getInitialClaimSeparationMultiplier())
-                                    inClanHomeRange = true;
-                            if (inClanHomeRange) {
-                                if (Clans.getConfig().isEnforceInitialClaimSeparation())
-                                    sender.sendMessage(TranslationUtil.getTranslation(sender.getUniqueID(), "commands.clan.claim.proximity_error", Clans.getConfig().getMinClanHomeDist() * Clans.getConfig().getInitialClaimSeparationMultiplier()).setStyle(TextStyles.RED));
-                                else if (PlayerDataManager.getClaimWarning(sender.getUniqueID()))
-                                    return claimChunk(sender, claimChunk, selectedClan, force);
-                                else {
-                                    sender.sendMessage(TranslationUtil.getTranslation(sender.getUniqueID(), "commands.clan.claim.proximity_warning", Clans.getConfig().getMinClanHomeDist() * Clans.getConfig().getInitialClaimSeparationMultiplier()).setStyle(TextStyles.YELLOW));
-                                    PlayerDataManager.setClaimWarning(sender.getUniqueID(), true);
-                                }
-                            } else
+        }
+        if(claimClan != null && !ClaimDataManager.getChunkPositionData(claimChunk).isBorderland()) {//In this scenario, we are always forcing the claim, so we should refund the clan the land is being taken from
+            claimClan.refundClaim();
+        }
+        if(selectedClan.isOpclan()) {
+            ClaimDataManager.swapChunk(claimChunk, claimOwner, selectedClan.getClanId());
+            sender.sendMessage(TranslationUtil.getTranslation(sender.getUniqueID(), "commands.clan.claim.success", selectedClan.getClanName()).setStyle(TextStyles.GREEN));
+            return true;
+        } else {
+            if (force || !Clans.getConfig().isForceConnectedClaims() || ChunkUtils.hasConnectedClaim(claimChunk, selectedClan.getClanId()) || selectedClan.getClaimCount() == 0) {
+                if (force || Clans.getConfig().getMaxClaims() <= 0 || selectedClan.getClaimCount() < selectedClan.getMaxClaimCount()) {
+                    if (selectedClan.getClaimCount() > 0)
+                        claimChunk(sender, claimChunk, selectedClan, force);
+                    else if (Clans.getConfig().getMinClanHomeDist() > 0 && Clans.getConfig().getInitialClaimSeparationMultiplier() > 0) {
+                        boolean inClanHomeRange = false;
+                        for (Map.Entry<Clan, BlockPos> pos : ClanCache.getClanHomes().entrySet())
+                            if (!pos.getKey().getClanId().equals(selectedClan.getClanId()) && pos.getKey().hasHome() && pos.getValue() != null && pos.getValue().getDistance(sender.getPosition().getX(), sender.getPosition().getY(), sender.getPosition().getZ()) < Clans.getConfig().getMinClanHomeDist() * Clans.getConfig().getInitialClaimSeparationMultiplier())
+                                inClanHomeRange = true;
+                        if (inClanHomeRange) {
+                            if (Clans.getConfig().isEnforceInitialClaimSeparation())
+                                sender.sendMessage(TranslationUtil.getTranslation(sender.getUniqueID(), "commands.clan.claim.proximity_error", Clans.getConfig().getMinClanHomeDist() * Clans.getConfig().getInitialClaimSeparationMultiplier()).setStyle(TextStyles.RED));
+                            else if (PlayerDataManager.getClaimWarning(sender.getUniqueID()))
                                 return claimChunk(sender, claimChunk, selectedClan, force);
+                            else {
+                                sender.sendMessage(TranslationUtil.getTranslation(sender.getUniqueID(), "commands.clan.claim.proximity_warning", Clans.getConfig().getMinClanHomeDist() * Clans.getConfig().getInitialClaimSeparationMultiplier()).setStyle(TextStyles.YELLOW));
+                                PlayerDataManager.setClaimWarning(sender.getUniqueID(), true);
+                            }
                         } else
                             return claimChunk(sender, claimChunk, selectedClan, force);
                     } else
-                        sender.sendMessage(TranslationUtil.getTranslation(sender.getUniqueID(), "commands.clan.claim.maxed", selectedClan.getClanName(), selectedClan.getMaxClaimCount()).setStyle(TextStyles.RED));
+                        return claimChunk(sender, claimChunk, selectedClan, force);
                 } else
-                    sender.sendMessage(TranslationUtil.getTranslation(sender.getUniqueID(), "commands.clan.claim.disconnected", selectedClan.getClanName()).setStyle(TextStyles.RED));
-            }
+                    sender.sendMessage(TranslationUtil.getTranslation(sender.getUniqueID(), "commands.clan.claim.maxed", selectedClan.getClanName(), selectedClan.getMaxClaimCount()).setStyle(TextStyles.RED));
+            } else
+                sender.sendMessage(TranslationUtil.getTranslation(sender.getUniqueID(), "commands.clan.claim.disconnected", selectedClan.getClanName()).setStyle(TextStyles.RED));
         }
         return false;
     }
@@ -102,7 +104,7 @@ public class ClanManagementUtil {
     public static boolean checkAndAttemptAbandon(EntityPlayerMP sender, Clan selectedClan, ChunkPositionWithData claimChunk, boolean isOpclanCommand, boolean force) {
         Chunk c = sender.getEntityWorld().getChunk(sender.getPosition());
         UUID claimOwnerClanId = ChunkUtils.getChunkOwner(c);
-        if(claimOwnerClanId != null) {
+        if(claimOwnerClanId != null && !ClaimDataManager.getChunkPositionData(claimChunk).isBorderland()) {
             Clan claimOwnerClan = ClanCache.getClanById(claimOwnerClanId);
             if(claimOwnerClan == null) {
                 ChunkUtils.clearChunkOwner(c);
