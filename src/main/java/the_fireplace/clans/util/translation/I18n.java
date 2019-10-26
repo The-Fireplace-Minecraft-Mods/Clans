1772f5f34b07e9c0c64c11808ca3000cac16d955
The_Fireplace <the.f1repl4ce@gmail.com>
2019-10-25 20:24:14 -0500

    Minor refactor

--- src/main/java/the_fireplace/clans/util/translation/I18n.java
+++ src/main/java/the_fireplace/clans/util/translation/I18n.java
@@ -5,11 +5,7 @@ class I18n {
     private static final ClansLanguageMap fallbackTranslator = new ClansLanguageMap("en_us");
 
     static String translateToLocalFormatted(String key, Object... format) {
-        return canTranslate(key) ? localizedName.translateKeyFormat(key, format) : translateToFallback(key, format);
-    }
-
-    private static String translateToFallback(String key, Object... format) {
-        return fallbackTranslator.translateKeyFormat(key, format);
+        return canTranslate(key) ? localizedName.translateKeyFormat(key, format) : fallbackTranslator.translateKeyFormat(key, format);
     }
 
     private static boolean canTranslate(String key) {
