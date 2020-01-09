79795eb43da95dd241d402737c7fffd0ce913a02
The_Fireplace <the.f1repl4ce@gmail.com>
2020-01-08 22:49:42 -0600

    Add tab completions for /opclan tp

--- src/main/java/the_fireplace/clans/commands/op/teleportation/OpCommandTeleport.java
+++ src/main/java/the_fireplace/clans/commands/op/teleportation/OpCommandTeleport.java
@@ -1,6 +1,8 @@
 package the_fireplace.clans.commands.op.teleportation;
 
+import com.google.common.collect.Lists;
 import mcp.MethodsReturnNonnullByDefault;
+import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
@@ -11,7 +13,10 @@ import the_fireplace.clans.util.EntityUtil;
 import the_fireplace.clans.util.TextStyles;
 import the_fireplace.clans.util.translation.TranslationUtil;
 
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Collections;
+import java.util.List;
 
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
@@ -49,5 +54,10 @@ public class OpCommandTeleport extends OpClanSubCommand {
                 EntityUtil.teleportHome(sender, home, targetClan.getHomeDim(), playerDim, false);
         } else
             sender.sendMessage(TranslationUtil.getTranslation(sender, "commands.clan.common.notfound", args[0]).setStyle(TextStyles.RED));
+    }
+
+    @Override
+    public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos) {
+        return args.length == 1 ? Lists.newArrayList(ClanCache.getClanNames().keySet()) : Collections.emptyList();
     }
 }
