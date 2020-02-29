58a75e75dce7a3c433e4c9b9593159ebae0257d0
The_Fireplace <the.f1repl4ce@gmail.com>
2020-02-29 13:29:52 -0600

    Set a minimum for kdrThreshold

--- src/main/java/the_fireplace/clans/ClansForge.java
+++ src/main/java/the_fireplace/clans/ClansForge.java
@@ -279,6 +279,7 @@ public final class ClansForge {
         @Config.Comment("If enabled, rewards will increase as a clan gets repeatedly defeated. This requires a compatible economy to be installed.")
         public static boolean increasingRewards = true;
         @Config.Comment("KDR threshold for decreasing the reward multiplier - Helps prevent rich clans from hiring people to purposely fail a raid and reduce their reward multiplier. This requires a compatible economy to be installed.")
+        @Config.RangeDouble(min=0)
         public static double kdrThreshold = 0.66;
         @Config.Comment("Formula to increase the reward multiplier to after a clan loses to raiders. m gets replaced with current multiplier. Supports operators + - * / **(exponent). WARNING: due to how this is evaluated, 2m =/= 2*m. With a previous multiplier of 5.5, 2m evaluates to 25.5. This requires increasingRewards to do anything.")
         public static String multiplierIncreaseFormula = "m**1.05";
