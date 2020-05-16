8b6f2675b85cb2fddbbe2358a01e38873e0281ab
The_Fireplace <the.f1repl4ce@gmail.com>
2020-05-15 22:21:16 -0500

    #214 /clan banner can now be used to decorate shields

--- src/main/java/the_fireplace/clans/commands/details/CommandBanner.java
+++ src/main/java/the_fireplace/clans/commands/details/CommandBanner.java
@@ -3,6 +3,7 @@ package the_fireplace.clans.commands.details;
 import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemBanner;
+import net.minecraft.item.ItemShield;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.JsonToNBT;
 import net.minecraft.nbt.NBTException;
@@ -63,6 +64,22 @@ public class CommandBanner extends ClanSubCommand {
 			ItemStack bannerStack = new ItemStack(banner);
 			bannerStack.setCount(count);
 			sender.setHeldItem(EnumHand.OFF_HAND, bannerStack);
+		} else if(sender.getHeldItemMainhand().getItem() instanceof ItemShield) {
+			ItemStack bannerStack = new ItemStack(banner);
+			NBTTagCompound bet = bannerStack.getSubCompound("BlockEntityTag");
+			NBTTagCompound finalBet = bet == null ? new NBTTagCompound() : bet.copy();
+			finalBet.setInteger("Base", bannerStack.getMetadata() & 15);
+			ItemStack shieldStack = sender.getHeldItemMainhand();
+			shieldStack.setTagInfo("BlockEntityTag", finalBet);
+			sender.setHeldItem(EnumHand.MAIN_HAND, shieldStack);
+		} else if(sender.getHeldItemOffhand().getItem() instanceof ItemShield) {
+			ItemStack bannerStack = new ItemStack(banner);
+			NBTTagCompound bet = bannerStack.getSubCompound("BlockEntityTag");
+			NBTTagCompound finalBet = bet == null ? new NBTTagCompound() : bet.copy();
+			finalBet.setInteger("Base", bannerStack.getMetadata() & 15);
+			ItemStack shieldStack = sender.getHeldItemOffhand();
+			shieldStack.setTagInfo("BlockEntityTag", finalBet);
+			sender.setHeldItem(EnumHand.OFF_HAND, shieldStack);
 		} else
 			sender.sendMessage(TranslationUtil.getTranslation(sender.getUniqueID(), "commands.clan.banner.player_nobanner").setStyle(TextStyles.RED));
 	}
